25eea96569a53514a9a8ebdd46eafa5573580eda
RkEclair <ryosukadnak@gmail.com>
2019-03-14 16:02:11 +0900

    Fix crashing

--- pages/index.js
+++ pages/index.js
@@ -2,53 +2,67 @@
  * @author RkEclair / https://github.com/RkEclair
  */
 
-import {Component} from "react";
-import * as bms from "bms";
-import toWav from "audiobuffer-to-wav";
+import {Component} from 'react';
+import * as bms from 'bms';
+import toWav from 'audiobuffer-to-wav';
 
 const Bouncer = async (reader, title, files, nameView) => {
   const bin = reader.result;
   const compiled = bms.Compiler.compile(bin);
   const notes = bms.Notes.fromBMSChart(compiled.chart);
   const sounds = bms.Keysounds.fromBMSChart(compiled.chart);
-  const bpm = compiled.chart.headers.get("bpm");
+  const bpm = compiled.chart.headers.get('bpm');
   const lastNote = notes.all()[notes.count() - 1];
-  const songLength = (((lastNote.endBeat || lastNote.beat) + 4) / bpm) * 60 * 44100;
+  const songLength =
+    (((lastNote.endBeat || lastNote.beat) + 4) / bpm) * 60 * 44100;
   const soundCache = {},
     decoder = new AudioContext(),
     ctx = new OfflineAudioContext(2, songLength, 44100);
-  const works = [];
   for (const note of notes.all()) {
-    const sound = sounds.get(note.keysound);
-    const soundFile = soundCache[sound.split(".")[0]];
+    const sound = sounds.get(note.keysound.replace('.ogg', '.wav'));
+    console.log(note.keysound, sound);
+    if (sound === undefined) {
+      if (
+        window.confirm(
+          `未定義のキー音 ${note.keysound} があります、続行しますか?`
+        )
+      )
+        continue;
+      nameView('中断');
+      return;
+    }
+    const soundFile = soundCache[sound.split('.')[0]];
     if (soundFile === undefined) {
       const soundReader = new FileReader();
-      soundCache[sound.split(".")[0]] = await new Promise((resolve) => {
-        soundReader.onload = async () => {
+      soundCache[sound.split('.')[0]] = new Promise((resolve) => {
+        soundReader.onload = () => {
           nameView(sound);
-          resolve(decoder.decodeAudioData(soundReader.result));
+          const buf = decoder.decodeAudioData(soundReader.result);
+          const source = ctx.createBufferSource();
+          source.src = buf;
+          source.connect(ctx.destination);
+          const offset = (note.beat / bpm) * 60 * 44100;
+          source.start(offset);
+          resolve();
         };
-        soundReader.readAsArrayBuffer(files[sound.split(".")[0]]);
+        soundReader.readAsArrayBuffer(files[sound.split('.')[0]]);
       });
     }
-    const source = ctx.createBufferSource();
-    source.src = buf;
-    source.connect(ctx.destination);
-    const offset = (note.beat / bpm) * 60 * 44100;
-    source.start(offset);
   }
-  nameView("");
-  await Promise.all(works);
+  nameView('合成中……');
+  await Promise.all(Object.values(soundCache));
+  nameView('Wav (PCM) に変換中……');
   const rendered = await ctx.startRendering();
+  nameView('完了');
   const buf = new Uint8Array(toWav(rendered));
-  const downloader = document.createElement("a");
-  downloader.download = title.split(".")[0] + ".wav";
+  const downloader = document.createElement('a');
+  downloader.download = title.split('.')[0] + '.wav';
   downloader.href = URL.createObjectURL(new Blob([buf]));
   downloader.click();
 };
 
 export default class Index extends Component {
-  state = {bouncing: false, loadingSoundName: ""};
+  state = {bouncing: false, loadingSoundName: ''};
 
   render() {
     return (
@@ -56,8 +70,8 @@ export default class Index extends Component {
         <h1>BMS Bouncer</h1>
         <h3>
           {this.state.bouncing
-            ? "バウンス中…"
-            : "BMSファイルが入ったフォルダ内のファイル全てを選択してね"}
+            ? 'バウンス中…'
+            : 'BMSファイルが入ったフォルダ内のファイル全てを選択してね'}
         </h3>
         <h4>{this.state.loadingSoundName}</h4>
         <input
@@ -69,10 +83,10 @@ export default class Index extends Component {
               const files = {};
               const firstBMSIndex = Object.keys(e.target.files).filter(
                 (key) => {
-                  files[e.target.files[key].name.split(".")[0]] =
+                  files[e.target.files[key].name.split('.')[0]] =
                     e.target.files[key];
-                  const exts = e.target.files[key].name.split(".");
-                  return ["bms", "bme", "bml", "pms"].some(
+                  const exts = e.target.files[key].name.split('.');
+                  return ['bms', 'bme', 'bml', 'pms'].some(
                     (e) => e === exts[exts.length - 1]
                   );
                 }
@@ -82,7 +96,7 @@ export default class Index extends Component {
               reader.onload = () => {
                 Bouncer(reader, bms.name, files, (name) =>
                   this.setState({bouncing: true, loadingSoundName: name})
-                ).then(() => this.setState({bouncing: false}));
+                ).finally(() => this.setState({bouncing: false}));
               };
               reader.readAsText(bms);
             }
