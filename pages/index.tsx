874d6df240933357884819a3570803e03018efc8
MikuroXina <ryosukadnak@gmail.com>
2021-12-18 19:45:19 +0900

    Complete bounce

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,23 @@ import { ChangeEvent, useState } from "react";
 import { NextPage } from "next";
 import toWav from "audiobuffer-to-wav";
 
-const forEachChannel = (
-  buf: AudioBuffer,
-  f: (arr: Float32Array, channel: number) => void,
+const writeBuffer = (
+  src: AudioBuffer,
+  dst: AudioBuffer,
+  offsetIndex: number,
 ) => {
-  for (let channel = 0; channel < buf.numberOfChannels; ++channel) {
-    const arr = buf.getChannelData(channel);
-    f(arr, channel);
+  for (let channel = 0; channel < dst.numberOfChannels; ++channel) {
+    const dstArray = dst.getChannelData(channel);
+    const srcArray = src.getChannelData(channel);
+    for (let index = 0; index < srcArray.length; index++) {
+      dstArray[index + offsetIndex] = srcArray[index];
+    }
   }
 };
+
+const sampleRate = 44100;
+
+const secondsToIndex = (seconds: number) => Math.round(seconds * sampleRate);
 
 const bounce = async (
   source: string,
@@ -22,86 +30,26 @@ const bounce = async (
   const bms = new BmsData(source, { free: () => {} });
   const lengthSeconds = bms.length_seconds();
   const ctx = new AudioContext();
-  const sampleRate = 44100;
   const buf = ctx.createBuffer(2, lengthSeconds * sampleRate, sampleRate);
-  const audioSeconds = bms.audio_play_seconds();
-  // const notes = bms.Notes.fromBMSChart(compiled.chart);
-  // const sounds = bms.Keysounds.fromBMSChart(compiled.chart);
-  // const bpm = compiled.chart.headers.get("bpm");
+  const audioSeconds: [string, number][] = bms.audio_play_seconds();
+  const audioCache: Record<string, AudioBuffer> = {};
+  for (const [filename, seconds] of audioSeconds) {
+    nameView(filename);
+    if (!audioCache[filename]) {
+      audioCache[filename] = await ctx.decodeAudioData(
+        await files[filename].arrayBuffer(),
+      );
+    }
+    const audio = audioCache[filename];
+    writeBuffer(audio, buf, secondsToIndex(seconds));
+  }
+  nameView("WAV に変換完了");
 
-  // const beat2Frame = (beat) => (beat / bpm) * 60 * 44100;
-
-  // const lastNote = notes.all()[notes.count() - 1];
-  // const songLength = beat2Frame((lastNote.endBeat || lastNote.beat) + 4);
-  // const soundCache = {},
-  //   soundLoaders = [],
-  //   decoder = new AudioContext(),
-  //   ctx = new OfflineAudioContext(2, songLength, 44100);
-  // for (const note of notes.all()) {
-  //   const sound = sounds.get(note.keysound.replace(/.ogg$/, ".wav"));
-  //   if (sound === undefined) {
-  //     if (
-  //       window.confirm(
-  //         `未定義のキー音 ${note.keysound} があります、続行しますか?`,
-  //       )
-  //     ) {
-  //       continue;
-  //     }
-  //     nameView("中断");
-  //     return;
-  //   }
-  //   const soundName = sound.split(".")[0];
-  //   if (soundCache[soundName] === undefined) {
-  //     soundLoaders.push(
-  //       new Promise((resolve) => {
-  //         const soundReader = new FileReader();
-  //         soundReader.onload = async () => {
-  //           nameView(sound);
-  //           const wav = await decoder.decodeAudioData(soundReader.result);
-  //           soundCache[soundName] = wav;
-  //           resolve();
-  //         };
-  //         soundReader.readAsArrayBuffer(files[sound.split(".")[0]]);
-  //       }),
-  //     );
-  //   }
-  // }
-  // nameView("キー音読み出し中……");
-  // await Promise.all(soundLoaders);
-
-  // const soundRings = [];
-  // for (const note of notes.all()) {
-  //   soundRings.push(async () => {
-  //     const sound = sounds.get(note.keysound.replace(/.ogg$/, ".wav"));
-  //     if (sound === undefined) return;
-  //     const soundName = sound.split(".")[0];
-
-  //     nameView("合成中…… " + soundName);
-  //     const buf = soundCache[soundName];
-  //     const source = ctx.createBufferSource();
-  //     source.buffer = buf;
-  //     source.connect(ctx.destination);
-  //     const startFrame = beat2Frame(note.beat);
-  //     source.start(startFrame);
-  //   });
-  // }
-  // nameView("合成中……");
-  // await Promise.all(soundRings);
-  // nameView("Wav (PCM) に変換中……");
-  // const rendered = await ctx.startRendering();
-
-  // const speaker = new window.AudioContext();
-  // const toListen = speaker.createBufferSource();
-  // toListen.buffer = rendered;
-  // toListen.connect(speaker.destination);
-  // toListen.start();
-
-  // nameView("完了");
-  // const buf = new Uint8Array(toWav(rendered, { float32: true }));
-  // const downloader = document.createElement("a");
-  // downloader.download = filename.split(".")[0] + ".wav";
-  // downloader.href = URL.createObjectURL(new Blob([buf]));
-  // downloader.click();
+  const rendered = new Uint8Array(toWav(buf, { float32: true }));
+  const downloader = document.createElement("a");
+  downloader.download = filename.split(".")[0] + ".wav";
+  downloader.href = URL.createObjectURL(new Blob([rendered]));
+  downloader.click();
 };
 
 const fileHandler =
