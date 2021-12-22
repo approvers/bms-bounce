513990a1540d95adf3583246fc58f693c261245b
MikuroXina <ryosukadnak@gmail.com>
2021-12-22 15:55:21 +0900

    Add header getter

--- ogg-wasm/src/lib.rs
+++ ogg-wasm/src/lib.rs
@@ -4,6 +4,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct OggDecoder {
+    reader: OggStreamReader<Cursor<Box<[u8]>>>,
     data: Vec<Vec<i16>>,
 }
 
@@ -18,7 +19,15 @@ impl OggDecoder {
             .expect("failed to read packet")
             .expect("audio stream not found");
         data.reverse();
-        Self { data }
+        Self { reader, data }
+    }
+
+    pub fn sample_rate(&self) -> u32 {
+        self.reader.ident_hdr.audio_sample_rate
+    }
+
+    pub fn num_of_channels(&self) -> u8 {
+        self.reader.ident_hdr.audio_channels
     }
 
     pub fn read_next_channel(&mut self) -> Option<Box<[f64]>> {
